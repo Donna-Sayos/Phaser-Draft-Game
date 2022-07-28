e999173b2aea83cdc36d5da5515f8ec782fcc530
Donna-Sayos <Donnasayos@gmail.com>
2022-07-28 01:05:20 -0400

    dough

--- js/game.js
+++ js/game.js
@@ -1,16 +1,23 @@
+let startScene = new Phaser.Scene('Title');
+
 const config = {
     type: Phaser.AUTO,
-    parent: 'game-container',
+    scale: {
+        parent: 'game-container',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        zoom: 1.5
+    },
     width: 800,
     height: 600,
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 300 }, 
-            debug: true
-        }
+            debug: true,
+        },
     },
     scene: {
+        startScene,
         preload: preload,
         create: create,
         update: update
@@ -19,11 +26,11 @@ const config = {
 
 let player;
 let iceCreams;
-let bombs;
+let poops;
 let platform1;
 let platform2;
-let platform3;
-let movingPlatform;
+let movingPlatform1;
+let movingPlatform2;
 let cursors;
 let score = 0;
 let gameOver = false;
@@ -35,8 +42,8 @@ function preload () {
     this.load.image('desert', 'public/desert.png');
     this.load.image('ground', 'public/desert-platform.png');
     this.load.image('iceCream', 'public/iceCream.png');
-    this.load.image('bomb', 'public/bomb.png');
-    this.load.spritesheet('baby', 'public/naked-baby.png', { frameWidth: 45, frameHeight: 60 }); //32, 48
+    this.load.image('poop', 'public/poop.png');
+    this.load.spritesheet('baby', 'public/naked-baby.png', { frameWidth: 50, frameHeight: 67 }); // 51, 65
 };
 
 function create () {
@@ -44,31 +51,31 @@ function create () {
     this.add.image(400, 300, 'desert');
 
     // adds the floor for the game;
-    platform1 = platform2 = platform3 = this.physics.add.staticGroup();
+    platform1 = platform2 = movingPlatform2 = this.physics.add.staticGroup();
     //  Here we create the ground. Scale it to fit the width of the game
     platform1 = this.physics.add.image(530, 650, 'ground').setScale(2).refreshBody().setImmovable(true); // floor;
-    platform1.body.setSize(400, 90, true);
+    platform1.body.setSize(530, 88, true);
     platform1.body.allowGravity = false;
 
 
     // adds some ledges
-    platform2 = this.physics.add.image(30, 350, 'ground').setImmovable(true); // mid ledge;
+    platform2 = this.physics.add.image(100, 350, 'ground').setImmovable(true); // mid ledge;
     platform2.body.setSize(275, 60, true);
     platform2.body.allowGravity = false;
 
-    platform3 = this.physics.add.image(780, 220, 'ground').setImmovable(true); // top ledge;
-    platform3.body.setSize(545, 50, true);
-    platform3.body.allowGravity = false;
+    movingPlatform1 = this.physics.add.image(650, 470, 'ground').setImmovable(true); // bot ledge;
+    movingPlatform1.body.setSize(420, 50, true);
+    movingPlatform1.body.allowGravity = false;
+    movingPlatform1.setVelocityX(50);
 
-    movingPlatform = this.physics.add.image(750, 470, 'ground').setImmovable(true); // bot ledge;
-
-    movingPlatform.body.setSize(420, 50, true);
-    movingPlatform.body.allowGravity = false;
-    movingPlatform.setVelocityX(50);
+    movingPlatform2 = this.physics.add.image(780, 220, 'ground').setImmovable(true); // top ledge;
+    movingPlatform2.body.setSize(545, 50, true);
+    movingPlatform2.body.allowGravity = false;
+    movingPlatform2.setVelocityY(50);
 
     // The player and its settings
-    player = this.physics.add.sprite(150, 350, 'baby'); //100, 450
-    player.body.setSize(30, 46, true);
+    player = this.physics.add.sprite(150, 350, 'baby'); //100, 450 || 150, 350
+    player.body.setSize(35, 51, true);
 
     //  Player physics properties. Give the little guy a slight bounce.
     player.setBounce(0.5);
@@ -84,7 +91,7 @@ function create () {
 
     this.anims.create({
         key: 'turn',
-        frames: [ { key: 'baby', frame: 0 } ],
+        frames: [ { key: 'baby', frames: 0 } ],
         frameRate: 20
     });
 
@@ -99,6 +106,7 @@ function create () {
     cursors = this.input.keyboard.createCursorKeys();
 
     //  Some iceCreams to collect, 14 in total, evenly spaced 70 pixels apart along the x axis
+    
     iceCreams = this.physics.add.group({
         key: 'iceCream',
         repeat: 13,
@@ -108,11 +116,11 @@ function create () {
     iceCreams.children.iterate(function (child) {
 
         //  Give each iceCream a slightly different bounce
-        child.setBounceY(Phaser.Math.FloatBetween(0.8, 0.9)); // 0.4, 0.8
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)); // 0.4, 0.8
 
     });
 
-    bombs = this.physics.add.group();
+    poops = this.physics.add.group();
 
     //  The score
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
@@ -120,23 +128,23 @@ function create () {
     //  Collide the player and the iceCreams with the platforms
     this.physics.add.collider(player, platform1);
     this.physics.add.collider(player, platform2);
-    this.physics.add.collider(player, platform3);
-    this.physics.add.collider(player, movingPlatform);
+    this.physics.add.collider(player, movingPlatform1);
+    this.physics.add.collider(player, movingPlatform2);
     
     this.physics.add.collider(iceCreams, platform1);
     this.physics.add.collider(iceCreams, platform2);
-    this.physics.add.collider(iceCreams, platform3);
-    this.physics.add.collider(iceCreams, movingPlatform);
+    this.physics.add.collider(iceCreams, movingPlatform1);
+    this.physics.add.collider(iceCreams, movingPlatform2);
 
-    this.physics.add.collider(bombs, platform1);
-    this.physics.add.collider(bombs, platform2);
-    this.physics.add.collider(bombs, platform3);
-    this.physics.add.collider(bombs, movingPlatform);
+    this.physics.add.collider(poops, platform1);
+    this.physics.add.collider(poops, platform2);
+    this.physics.add.collider(poops, movingPlatform1);
+    this.physics.add.collider(poops, movingPlatform2);
 
     //  Checks to see if the player overlaps with any of the iceCreams, if he does call the collecticeCream function
     this.physics.add.overlap(player, iceCreams, collecticeCream, null, this);
 
-    this.physics.add.collider(player, bombs, hitBomb, null, this);
+    this.physics.add.collider(player, poops, hitpoop, null, this);
 };
 
 function update () {
@@ -159,16 +167,19 @@ function update () {
     }
 
     if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-330); // -330
+        player.setVelocityY(-360); // -330
     }
 
-    if (movingPlatform.x >= 500) // 500
-    {
-        movingPlatform.setVelocityX(-90); // -50
+    if (movingPlatform1.x >= 500) {
+        movingPlatform1.setVelocityX(-90); 
+    } else if (movingPlatform1.x <= 300) {
+        movingPlatform1.setVelocityX(90); 
     }
-    else if (movingPlatform.x <= 300) // 300
-    {
-        movingPlatform.setVelocityX(90); // 50
+
+    if (movingPlatform2.y >= 300) {
+        movingPlatform2.setVelocityY(-50); 
+    } else if (movingPlatform2.y <= 200) {
+        movingPlatform2.setVelocityY(50); 
     }
 };
 
@@ -176,10 +187,10 @@ function collecticeCream (player, iceCream) {
     iceCream.disableBody(true, true);
 
     //  Add and update the score
-    score += 10;
+    score += 20;
     scoreText.setText('Score: ' + score);
 
-    if (iceCreams.countActive(true) === 0) {
+    if (iceCreams.countActive(true) === 1) {
         //  A new batch of iceCreams to collect
         iceCreams.children.iterate(function (child) {
 
@@ -187,18 +198,18 @@ function collecticeCream (player, iceCream) {
 
         });
 
-        const x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+            let x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
 
-        const bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        bomb.allowGravity = false;
+        let poop = poops.create(x, 16, 'poop');
+        poop.setBounce(1);
+        poop.setCollideWorldBounds(true);
+        poop.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        poop.allowGravity = false;
 
     }
 };
 
-function hitBomb (player, bomb) {
+function hitpoop (player, poop) {
     this.physics.pause();
 
     player.setTint(0xff0000);
---
+++ js/scenes/StartScene.js
@@ -0,0 +1,11 @@
+class StartScene extends Phaser.Scene {
+    constructor () {
+        super({ key: "StartScene" });
+    }
+
+    preload () {
+        this.load.image('background', );
+    }
+};
+
+export default StartScene;
