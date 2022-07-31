2b704831c25b7bb2b784a8e64353d0ac8f4e284d
Donna-Sayos <Donnasayos@gmail.com>
2022-07-30 22:25:04 -0400

    heroku trials

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: 'js/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+    ],
+  },
+};
