4fd2007551f626a35c2c814d966e0aa855320b43
Donna-Sayos <Donnasayos@gmail.com>
2022-07-30 22:34:17 -0400

    yaa

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HTML_FILE = path.join('/index.html');
 
 app.use(express.static(DIST_DIR));
 app.get('*', (req, res) => {
-  res.sendFile(DIST_DIR + HTML_FILE);
+  res.sendFile(HTML_FILE);
 });
 
 const PORT = process.env.PORT || 8080;
