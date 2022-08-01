728d7c5bcef222d972313889a2d7d887fed9c98d
Donna-Sayos <Donnasayos@gmail.com>
2022-07-31 20:44:03 -0400

    some changes

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-const DIST_DIR = path.join(__dirname, '/dist');
-const HTML_FILE = path.join('/index.html');
 
 // logging middleware
 app.use(morgan('dev'));
@@ -13,12 +11,14 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 // static file-serving middleware
-app.use(express.static(__dirname, '..', './index.html'));
+app.use(express.static(path.join(__dirname, './index.html')));
 
 // sends index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', './index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', './index.html'));
+// });
+
+// app.use(express.static(path.join(__dirname, './dist')));
 
 // error handling endware
 app.use((err, req, res, next) => {
